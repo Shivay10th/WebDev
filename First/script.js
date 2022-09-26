dfd7820441fc9e7ec765066ce4cef68ff8a14da0
shivay10th <shivay9455@gmail.com>
2022-09-26 10:05:49 +0530

    First commit

---
+++ First/script.js
@@ -0,0 +1,13 @@
+/** @format */
+
+function changeColor(color) {
+	document.body.style.backgroundColor = color;
+}
+
+function randomColor() {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	document.body.style.backgroundColor =
+		'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
